5a2f7b2ac41040036ba2efaef5ca1f17e30e0929
daviduzondu <uzondudavid2004@gmail.com>
2023-04-16 19:27:07 +0100

    Made a few changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -34,8 +34,8 @@ function Container() {
                 <Control label="Color" type="color" value={boxShadow[5]} onChangeHandler={e => onChangeHandler(e, 5)} />
                 <Control label="Offset X" type="range" value={boxShadow[0]} onChangeHandler={e => onChangeHandler(e, 0)} />
                 <Control label="Offset Y" type="range" value={boxShadow[1]} onChangeHandler={e => onChangeHandler(e, 1)} />
-                <Control label="Spread" type="range" value={boxShadow[2]} onChangeHandler={e => onChangeHandler(e, 2)} />
-                <Control label="Blur" type="range" value={boxShadow[3]} onChangeHandler={e => onChangeHandler(e, 3)} />
+                <Control label="Blur" type="range" value={boxShadow[2]} onChangeHandler={e => onChangeHandler(e, 2)} />
+                <Control label="Spread" type="range" value={boxShadow[3]} onChangeHandler={e => onChangeHandler(e,3)} />
                 <Control label="Inset" type="checkbox" value={boxShadow[4]} onChangeHandler={e => onChangeHandler(e, 4)} />
             </div>
             <div>
