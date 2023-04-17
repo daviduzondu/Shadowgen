9f4a9e95f63dc51d3d896764b8eef4e7b2e21670
daviduzondu <uzondudavid2004@gmail.com>
2023-04-17 17:00:34 +0100

    Made a few changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Container from './components/Container'
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      console.log(window)
+      setWindowWidth(window.innerWidth)
+    })
+  }, [windowWidth])
+
   return (
     <div className="App">
-      <Header />
-      <div className='mainApp'>
-        <Container />
-      </div>
+      {
+        window.innerWidth > 680 ?
+          <>
+            <Header />
+            <div className='mainApp'>
+              <Container />
+            </div>
+          </>
+          :
+          <div className='responsive'>
+
+            <div>
+            <h1>
+              Your {window.clientInformation.userAgentData.platform} device screen is not supported.
+            </h1>
+              Please run this on desktop or a device with bigger screen.
+              
+            </div>
+          </div>
+      }
     </div>
   )
 }
