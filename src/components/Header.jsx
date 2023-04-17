73dcb5e40f0bfb35ecebac865e9bb4b22ef01dab
daviduzondu <uzondudavid2004@gmail.com>
2023-04-17 17:54:15 +0100

    Added a star on Github button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div className='Header'>
             <div>SHADOWGEN</div>
+            <a class="github-button" href="https://github.com/daviduzondu/shadowgen" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star daviduzondu/shadowgen on GitHub">Star on Github</a>
         </div>
     )
 }
