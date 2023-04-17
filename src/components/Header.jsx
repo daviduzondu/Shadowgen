d61e2d35e67f64687d8fd10e0a7a959ef829f448
daviduzondu <uzondudavid2004@gmail.com>
2023-04-17 18:00:14 +0100

    Added a star on Github button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GitHubButton from 'react-github-btn'
 
 function Header() {
     return (
         <div className='Header'>
             <div>SHADOWGEN</div>
-            <a class="github-button" href="https://github.com/daviduzondu/shadowgen" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star daviduzondu/shadowgen on GitHub">Star on Github</a>
+            <GitHubButton href="https://github.com/daviduzondu/shadowgen" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star daviduzondu/shadowgen on GitHub">Star</GitHubButton>
         </div>
     )
 }
